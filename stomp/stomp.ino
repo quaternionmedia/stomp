2a7c4856693d8f905b35c6a1fbc4ea9947842b43
Harpo <harpo@harpo.me>
2021-07-29 19:54:10 -0700

    simplify and speed up code

--- stomp/stomp.ino
+++ stomp/stomp.ino
@@ -1,8 +1,3 @@
- /* 
-Shamelessly copied from:
-MehranMaleki: Arduino Examples
-https://electropeak.com/learn/
-*/
 
 
 //Mux control pins
@@ -10,28 +5,10 @@ int s0 = 8;
 int s1 = 7;
 int s2 = 6;
 int s3 = 5;
-
+int controlPin[] = {s0, s1, s2, s3};
+ 
 //Mux in "SIG" pin
 int SIG_pin = 15;
-
-int muxChannel[16][4]={
-  {0,0,0,0}, //channel 0
-  {1,0,0,0}, //channel 1
-  {0,1,0,0}, //channel 2
-  {1,1,0,0}, //channel 3
-  {0,0,1,0}, //channel 4
-  {1,0,1,0}, //channel 5
-  {0,1,1,0}, //channel 6
-  {1,1,1,0}, //channel 7
-  {0,0,0,1}, //channel 8
-  {1,0,0,1}, //channel 9
-  {0,1,0,1}, //channel 10
-  {1,1,0,1}, //channel 11
-  {0,0,1,1}, //channel 12
-  {1,0,1,1}, //channel 13
-  {0,1,1,1}, //channel 14
-  {1,1,1,1}  //channel 15
-};
 
 void setup(){
   pinMode(s0, OUTPUT); 
@@ -49,29 +26,22 @@ void setup(){
 
 
 void loop(){
-
+  
   //Loop through and read all 16 values
-  for(int i = 0; i < 16; i ++){
-//    Serial.print("Value at channel ");
-    Serial.print(i);
-    Serial.print(" ");
+  for(int i = 0; i < 16; i++){
     Serial.print(readMux(i));
-    
+    Serial.print(" ");
   }
   Serial.println();
   delay(10);
-
 }
 
 
-float readMux(int channel){
-  int controlPin[] = {s0, s1, s2, s3};
-
-
-  //loop through the 4 sig
-  for(int i = 0; i < 4; i ++){
-    digitalWrite(controlPin[i], muxChannel[channel][i]);
-  }
+float readMux(int channel) {
+  digitalWrite(s0, channel & 0b0001);
+  digitalWrite(s1, channel & 0b0010);
+  digitalWrite(s2, channel & 0b0100);
+  digitalWrite(s3, channel & 0b1000);
 
   //read the value at the SIG pin
   int val = analogRead(SIG_pin);
