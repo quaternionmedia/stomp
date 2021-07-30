e34fb3b710666ac601b5e34e19a88eb2161e3f97
Harpo <harpo@harpo.me>
2021-07-29 20:19:41 -0700

    working MIDI prototype!!

--- stomp/stomp.ino
+++ stomp/stomp.ino
@@ -1,4 +1,4 @@
-
+#include "MIDIUSB.h"
 
 //Mux control pins
 int s0 = 8;
@@ -6,9 +6,10 @@ int s1 = 7;
 int s2 = 6;
 int s3 = 5;
 int controlPin[] = {s0, s1, s2, s3};
- 
+
 //Mux in "SIG" pin
 int SIG_pin = 15;
+int STATE[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 void setup(){
   pinMode(s0, OUTPUT); 
@@ -28,8 +29,15 @@ void setup(){
 void loop(){
   
   //Loop through and read all 16 values
-  for(int i = 0; i < 16; i++){
-    Serial.print(readMux(i));
+  for(int i = 0; i < 16; i++) {
+    int mux = readMux(i);
+    if (STATE[i] != mux) {
+      midiEventPacket_t packet = { 0x09, 0x90, 0x3C + i, (uint8_t) mux };
+      MidiUSB.sendMIDI(packet);
+      MidiUSB.flush();
+      STATE[i] = mux;
+    }
+    Serial.print(mux);
     Serial.print(" ");
   }
   Serial.println();
@@ -45,8 +53,8 @@ float readMux(int channel) {
 
   //read the value at the SIG pin
   int val = analogRead(SIG_pin);
-
+  
   //return the value
-  float voltage = (val * 5.0) / 1024.0;
-  return voltage;
+//  float voltage = (val * 5.0) / 1024.0;
+  return val >> 3;
 }
